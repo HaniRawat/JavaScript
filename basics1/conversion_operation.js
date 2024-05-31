25849bc502b1985b66a8357bb7c47e4c9b712c34
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-05-31 14:36:47 +0000

    yo

---
+++ basics1/conversion_operation.js
@@ -0,0 +1,18 @@
+let score = "33abc" // gives NaN
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) 
+// console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//OPERATIONS
+
